cbb4a4335d90273b5e372a784907a9e860efaff6
Tatiane Navarro <tatianenavarro@pecege.com>
2021-07-24 19:23:50 -0300

    test: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { ValidationBuilder as Builder } from '@/validation/builder/validation-builder'
+import { ValidationComposite } from '@/validation/validators'
+import { makeSignUpValidation } from './signup-validation-factory'
+
+describe('SignUpValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
