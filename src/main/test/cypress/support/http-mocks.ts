f7b2efe3e1c83b9e376b27ea3921c8196c233171
Tatiane Navarro <tatianenavarro@pecege.com>
2021-07-25 15:11:34 -0300

    refactor: move duplicated code to helper methods

---
+++ src/main/test/cypress/support/http-mocks.ts
@@ -0,0 +1,27 @@
+import faker from 'faker'
+import { Method } from 'axios'
+
+export const mockInvalidCredentialsError = (url: RegExp): void => {
+  cy.intercept('POST', url, {
+    statusCode: 401,
+    body: {
+      error: faker.random.words()
+    }
+  }).as('request')
+}
+
+export const mockUnexpectedError = (url: RegExp, method: Method): void => {
+  cy.intercept(method, url, {
+    statusCode: faker.helpers.randomize([400, 404, 500]),
+    body: {
+      error: faker.random.words()
+    }
+  }).as('request')
+}
+
+export const mockOk = (url: RegExp, method: Method, response: any): void => {
+  cy.intercept(method, url, {
+    statusCode: 200,
+    body: response
+  }).as('request')
+}
