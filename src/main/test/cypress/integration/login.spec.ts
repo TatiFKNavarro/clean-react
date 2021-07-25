127ddbbd423c851070f0e735cc8ab8ab2a08a014
Tatiane Navarro <tatianenavarro@pecege.com>
2021-07-25 13:50:26 -0300

    refactor: change test to use faker

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -76,22 +76,6 @@ describe('Login', () => {
     cy.url().should('eq', `${baseUrl}/login`)
   })
 
-  it('Should present save accessToken if valid credentials are provided', () => {
-    cy.intercept('POST', /login/, {
-      statusCode: 200,
-      body: {
-        accessToken: faker.datatype.uuid()
-      }
-    })
-    cy.getByTestId('email').focus().type('mango@gmail.com')
-    cy.getByTestId('password').focus().type('12345')
-    cy.getByTestId('submit').click()
-    cy.getByTestId('main-error').should('not.exist')
-    cy.getByTestId('spinner').should('not.exist')
-    cy.url().should('eq', `${baseUrl}/`)
-    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
-  })
-
   it('Should present UnexpectedError if invalid data is returned', () => {
     cy.intercept('POST', /login/, {
       statusCode: 200,
@@ -105,5 +89,21 @@ describe('Login', () => {
     cy.getByTestId('spinner').should('not.exist')
     cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
     cy.url().should('eq', `${baseUrl}/login`)
+  })
+
+  it('Should present save accessToken if valid credentials are provided', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.datatype.uuid()
+      }
+    })
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('main-error').should('not.exist')
+    cy.getByTestId('spinner').should('not.exist')
+    cy.url().should('eq', `${baseUrl}/`)
+    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
 })
