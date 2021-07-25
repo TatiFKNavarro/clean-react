6b700f64a7a7cc5aae6ccd65da89b4da699d0a6d
Tatiane Navarro <tatianenavarro@pecege.com>
2021-07-25 15:30:16 -0300

    test: ensure signup present correct error if server fails

--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -1,3 +1,4 @@
 import * as Helper from './http-mocks'
 
 export const mockEmailInUseError = (): void => Helper.mockInvalidCredentialsError(/signup/)
+export const mockUnexpectedError = (): void => Helper.mockUnexpectedError(/signup/, 'POST')
