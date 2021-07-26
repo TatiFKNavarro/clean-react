03d1103877fbeb570514eb7fcdc910e44e3fc0fb
Tatiane Navarro <tatianenavarro@pecege.com>
2021-07-26 20:38:10 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -21,42 +21,7 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver Resultado</footer>
           </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
-              <time>
-                <span className={Styles.day}>29</span>
-                <span className={Styles.month}>07</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
-              <time>
-                <span className={Styles.day}>29</span>
-                <span className={Styles.month}>07</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
-              <time>
-                <span className={Styles.day}>29</span>
-                <span className={Styles.month}>07</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
+          <li></li>
         </ul>
       </div>
       <Footer />
