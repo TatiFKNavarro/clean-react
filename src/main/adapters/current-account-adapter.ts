e9f8441371fb3d979b343b64990472cea32e0269
Tati Navarro <tatinavarro@outlook.com.br>
2021-07-29 08:06:57 -0300

    feat: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -8,3 +8,7 @@ export const setCurrentAccountAdapter = (account: AccountModel): void => {
   }
   makeLocalStorageAdapter().set('account', account)
 }
+
+export const getCurrentAccountAdapter = (): void => {
+  return makeLocalStorageAdapter().get('account')
+}
