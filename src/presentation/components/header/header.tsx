d722fb146b97edbc93b608b26eb0a4b12fc88283
Tati Navarro <tatinavarro@outlook.com.br>
2021-08-01 11:22:32 -0300

    feat: ensure header renders username correctly

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -5,7 +5,7 @@ import { ApiContext } from '@/presentation/contexts'
 import Styles from './header-styles.scss'
 
 export const Header: React.FC = () => {
-  const { setCurrentAccount } = useContext(ApiContext)
+  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext)
   const history = useHistory()
 
   const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
@@ -19,7 +19,7 @@ export const Header: React.FC = () => {
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
-          <span>Tati</span>
+          <span data-testid="username">{getCurrentAccount().name}</span>
           <a data-testid="logout" href="#" onClick={logout}>Sair</a>
         </div>
       </div>
