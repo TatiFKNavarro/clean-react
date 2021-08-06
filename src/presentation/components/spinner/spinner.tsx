1f7cd37f0aac49b9d0cd228870eeb1c6edf1e30f
Tatiane Navarro <tatianenavarro@pecege.com>
2021-08-06 10:27:23 -0300

    feat: add SurveyResult layout

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Styles from './spinner-styles.scss'
 
-type Props = React.HTMLAttributes<HTMLDivElement>
+type Props = React.HTMLAttributes<HTMLDivElement> & {
+  isNegative?: boolean
+}
 
 const Spinner: React.FC<Props> = (props: Props) => {
+  const negativeClass = props.isNegative ? Styles.negative : ''
+
   return (
-    <div data-testid="spinner" {...props} className={[Styles.spinner, props.className].join(' ')}>
+    <div data-testid="spinner" {...props} className={[Styles.spinner, negativeClass, props.className].join(' ')}>
       <div /><div /><div /><div />
     </div>
   )
