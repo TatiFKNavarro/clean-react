70c75c480a2c550b4f67b2e2d7bb67d2e74b470e
Tatiane Navarro <tatianenavarro@pecege.com>
2021-08-09 18:25:40 -0300

    feat: add SurveyResult composition

---
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { SurveyResult } from '@/presentation/pages'
+import { makeRemoteLoadSurveyResult } from '@/main/factories/usecases'
+
+export const makeSurveyResult: React.FC = () => {
+  const { id } = useParams()
+  return (
+    <SurveyResult
+      loadSurveyResult={makeRemoteLoadSurveyResult(id)}
+    />
+  )
+}
