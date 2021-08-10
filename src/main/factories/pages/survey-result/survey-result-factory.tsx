1c8f49965f2c1f7994901d234cdc00f26924a063
Tatiane Navarro <tatianenavarro@pecege.com>
2021-08-10 20:47:57 -0300

    refactor: update libs

--- src/main/factories/pages/survey-result/survey-result-factory.tsx
+++ src/main/factories/pages/survey-result/survey-result-factory.tsx
@@ -3,12 +3,11 @@ import { useParams } from 'react-router-dom'
 import { SurveyResult } from '@/presentation/pages'
 import { makeRemoteLoadSurveyResult, makeRemoteSaveSurveyResult } from '@/main/factories/usecases'
 
-type RouteParamsType = {
-  id: string
-}
-
 export const makeSurveyResult: React.FC = () => {
-  const { id } = useParams<RouteParamsType>()
+  type Props = {
+    id: string
+  }
+  const { id } = useParams<Props>()
   return (
     <SurveyResult
       loadSurveyResult={makeRemoteLoadSurveyResult(id)}
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,8 @@ const { DefinePlugin } = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-const common = require('./webpack.common')
 const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
 
 module.exports = merge(common, {
   mode: 'production',
