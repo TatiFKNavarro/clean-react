56cea57a0b49c9ca6bb69cdb4bba9664608a12d7
Tatiane Navarro <tatianenavarro@pecege.com>
2021-08-09 21:00:12 -0300

    test: ensure SurveyResult presents error if request fails

---
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -0,0 +1,20 @@
+import * as Helper from '../utils/helpers'
+import * as Http from '../utils/http-mocks'
+
+const path = /api\/surveys/
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
+
+describe('SurveyResult', () => {
+  beforeEach(() => {
+    cy.fixture('account').then(account => {
+      Helper.setLocalStorageItem('account', account)
+    })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    mockUnexpectedError()
+    console.log(path)
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+})
